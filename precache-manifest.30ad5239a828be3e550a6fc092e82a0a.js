546dbb7329c6ac5be88bff5f80c7e8ca248181f4
Joe Frazier <frazierj@wustl.edu>
2020-08-24 16:40:09 -0500

    Header and blog section

---
+++ precache-manifest.30ad5239a828be3e550a6fc092e82a0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444967d1c891f4137b79c888801a11d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b803e7bfb2092f9457c9",
+    "url": "/static/css/main.0f29853e.chunk.css"
+  },
+  {
+    "revision": "820342efa8c2c904dab9",
+    "url": "/static/js/2.e8a6e4c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e8a6e4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b803e7bfb2092f9457c9",
+    "url": "/static/js/main.e285b2a7.chunk.js"
+  },
+  {
+    "revision": "54b4d8775c824ebc0c41",
+    "url": "/static/js/runtime-main.d85fd4c2.js"
+  },
+  {
+    "revision": "4501a60c1d5ffb4b01d2ae258381e6c2",
+    "url": "/static/media/background1.4501a60c.gif"
+  }
+]);
