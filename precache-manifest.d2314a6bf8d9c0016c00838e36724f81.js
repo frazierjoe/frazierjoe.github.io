9f7e6b1832ee29f995c0a9554f392c851e7faeeb
Joe Frazier <frazierj@wustl.edu>
2020-09-04 21:42:56 -0500

    Blog

---
+++ precache-manifest.d2314a6bf8d9c0016c00838e36724f81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8a0a1b70b109b076600ce872acb866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d33e9e232739736e53a",
+    "url": "/static/css/main.bcf57ae9.chunk.css"
+  },
+  {
+    "revision": "6242a06b31b4320627f8",
+    "url": "/static/js/2.423f9807.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.423f9807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d33e9e232739736e53a",
+    "url": "/static/js/main.4f3ed22f.chunk.js"
+  },
+  {
+    "revision": "54b4d8775c824ebc0c41",
+    "url": "/static/js/runtime-main.d85fd4c2.js"
+  },
+  {
+    "revision": "0888545967c33b49c4a3aacb374f9622",
+    "url": "/static/media/consumerSpending.08885459.png"
+  }
+]);
